6afac900e154069c4f70087636937a8ff7cdac49
陈欢 <jingfeng_quietwind@163.com>
2018-09-22 14:58:50 +0800

    change ts

--- helper.ts
+++ helper.ts
@@ -1,20 +1,20 @@
-import { resolve, basename, extname } from "path";
-import { sync } from "glob";
+import { resolve, basename, extname } from "path"
+import { sync } from "glob"
 
-import { Entry } from "webpack";
+import { Entry } from "webpack"
 
 export function GetAllEntries(): Entry {
-  const files: string[] = sync(resolve(__dirname, "./src/entries/*.tsx"));
-  const entry: Entry = {};
+    const files: string[] = sync(resolve(__dirname, "./src/entries/*.tsx"))
+    const entry: Entry = {}
 
-  files.forEach(file => {
-    const name = basename(file, extname(file));
-    entry[name] = [file];
-  });
+    files.forEach((file) => {
+        const name = basename(file, extname(file))
+        entry[name] = [ file ]
+    })
 
-  return entry;
+    return entry
 }
 
 export function IsProd(): boolean {
-  return process.env.NODE_ENV === "production";
+    return process.env.NODE_ENV === "production"
 }

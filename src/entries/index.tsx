203a89b356137b1328de5b136088073352715708
陈欢 <jingfeng_quietwind@163.com>
2018-09-23 12:54:11 +0800

    change folder name

--- src/entries/index.tsx
+++ src/entries/index.tsx
@@ -4,6 +4,6 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 import "../scss/reset.scss"
-import Index from "../pages/Index"
+import Index from "../pages/index"
 
 ReactDOM.render(<Index />, document.getElementById("reactApp"))

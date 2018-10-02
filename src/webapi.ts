c15863f39e52c18fb073db83f9cbf79c974301ec
陈欢 <jingfeng_quietwind@163.com>
2018-10-02 12:24:39 +0800

    add config setting

--- src/webapi.ts
+++ src/webapi.ts
@@ -1,17 +1,22 @@
-import axios from "axios"
+import axios, { AxiosRequestConfig } from "axios"
 
-function getAPIurl(method: string) {
+const config: AxiosRequestConfig = {}
+
+function getApiUrl(method: string) {
     const HOST = "127.0.0.1:3001"
+
     return `//${HOST}/${method}`.toLocaleLowerCase()
 }
 
 function onError(res) {
     // api request on error
     console.error(res)
+
+    return res
 }
 
-export default function Webapi<T>(method: string, payload: any) {
-    return axios.post<T>(getAPIurl(method), payload, {}).then((res) => {
+export default function WebApi<T>(method: string, payload: any): Promise<void | T> {
+    return axios.post<T>(getApiUrl(method), payload, config).then((res) => {
         return res.data
     }, onError)
 }
